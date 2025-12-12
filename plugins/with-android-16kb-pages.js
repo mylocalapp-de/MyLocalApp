89cb20d3a90d7bd32138de294431da7012518043
pascalkienast <info@pascalkienast.de>
2025-12-12 16:42:48 +0100

    Remove deprecated gradle property from 16KB plugin

--- plugins/with-android-16kb-pages.js
+++ plugins/with-android-16kb-pages.js
@@ -1,32 +1,14 @@
-const { withGradleProperties, withAppBuildGradle } = require('@expo/config-plugins');
+const { withAppBuildGradle } = require('@expo/config-plugins');
 
 /**
  * Config plugin to enable 16KB page size support for Android 15+
  * Required by Google Play as of 2024
+ * 
+ * Sets useLegacyPackaging = false which ensures native libs are stored
+ * uncompressed and page-aligned in the APK/AAB.
  */
 function with16KBPages(config) {
-  // Add gradle.properties entries
-  config = withGradleProperties(config, (config) => {
-    // Remove any existing entries to avoid duplicates
-    config.modResults = config.modResults.filter(
-      (item) =>
-        !(item.type === 'property' && 
-          (item.key === 'android.bundle.enableUncompressedNativeLibs' ||
-           item.key === 'android.experimental.enableNewResourceShrinker.preciseShrinking'))
-    );
-
-    // Add the properties for 16KB page support
-    config.modResults.push({
-      type: 'property',
-      key: 'android.bundle.enableUncompressedNativeLibs',
-      value: 'false',
-    });
-
-    return config;
-  });
-
-  // Add packaging options to app/build.gradle (AGP 8.0+ syntax)
-  config = withAppBuildGradle(config, (config) => {
+  return withAppBuildGradle(config, (config) => {
     if (config.modResults.contents.includes('useLegacyPackaging')) {
       return config;
     }
@@ -47,8 +29,6 @@ function with16KBPages(config) {
 
     return config;
   });
-
-  return config;
 }
 
 module.exports = with16KBPages;
